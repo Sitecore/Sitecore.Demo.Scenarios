13e6b7a6e86bbc49f26b82608e61260abc73d89c
Christos Papoutsakis <chpa@sitecore.net>
2024-03-05 13:14:14 +0200

    Add productIcons helper object

---
+++ src/frontend/helpers/scenario/index.ts
@@ -0,0 +1,24 @@
+import { ProductOptions } from '@/interfaces/scenario';
+
+export const productIcons = {
+  [ProductOptions.taxonomy_productoptions_cdp]:
+    'https://ch.sitecoredemo.com/api/public/content/82ae0487035a4171ba3ae5d4146644a1?v=df4fc96e',
+  [ProductOptions.taxonomy_productoptions_connect]:
+    'https://ch.sitecoredemo.com/api/public/content/56f38285a2794e9989eda2d9d8870955?v=e787ee1b',
+  [ProductOptions.taxonomy_productoptions_contentHub]:
+    'https://ch.sitecoredemo.com/api/public/content/b9a88ea1cb674ff197c16306b6f071ee?v=b274d31c',
+  [ProductOptions.taxonomy_productoptions_contentHubOne]:
+    'https://ch.sitecoredemo.com/api/public/content/2a06129658524773a1b12983349dc1f3?v=8241aed8',
+  [ProductOptions.taxonomy_productoptions_discover]:
+    'https://ch.sitecoredemo.com/api/public/content/779dd4820b1e4c5683d150741823f8e2?v=870d151c',
+  [ProductOptions.taxonomy_productoptions_ordercloud]:
+    'https://ch.sitecoredemo.com/api/public/content/07983393aead4243a8e82be5c2b24cdc?v=04c6f555',
+  [ProductOptions.taxonomy_productoptions_personalize]:
+    'https://ch.sitecoredemo.com/api/public/content/29f243787f514f248b23dc63a5d3fe71?v=e161831c',
+  [ProductOptions.taxonomy_productoptions_search]:
+    'https://ch.sitecoredemo.com/api/public/content/deb827ca52054297b694721e48afef05?v=06658994',
+  [ProductOptions.taxonomy_productoptions_send]:
+    'https://ch.sitecoredemo.com/api/public/content/1394ed8cd2a04895902768c43a3b14bb?v=b5e09410',
+  [ProductOptions.taxonomy_productoptions_xmCloud]:
+    'https://ch.sitecoredemo.com/api/public/content/4eab402a23724aafb9813fe73adcc826?v=0381a9f3',
+};
