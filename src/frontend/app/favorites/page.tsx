594cf1fd73b93b6e7b74963f9d932a5ee7bec8f4
Christos Papoutsakis <chpa@sitecore.net>
2024-03-06 17:09:39 +0200

    Update favorites page

--- src/frontend/app/favorites/page.tsx
+++ src/frontend/app/favorites/page.tsx
@@ -1,4 +1,18 @@
 // `app/favorites/page.tsx` is the UI for the `/favorites` URL
-export default function FavoritesPage() {
-  return <h1>Hello, Favorites Page!</h1>;
+import { getAllScenarios } from '@/api/queries/scenarios';
+import SavedScenarioGrid from '../components/SavedScenarioGrid';
+
+export default async function FavoritesPage() {
+  const scenarios = await getAllScenarios();
+
+  return (
+    <main>
+      <section className="mt-16 ml-64 sticky">
+        <h1 className="text-5xl font-bold text-black-light">Saved</h1>
+      </section>
+      <section className="flex flex-row gap-6 ml-64 mt-10">
+        {scenarios && scenarios.length > 0 && <SavedScenarioGrid scenarios={scenarios} />}
+      </section>
+    </main>
+  );
 }
