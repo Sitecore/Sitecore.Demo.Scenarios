c6e83c8161110bec19a4518664ad74ab4f870aca
Neli Kostadinova <nko@sitecore.net>
2024-03-18 09:58:24 +0200

    some fixes

--- src/frontend/app/not-found.tsx
+++ src/frontend/app/not-found.tsx
@@ -3,16 +3,16 @@ import Image from 'next/image';
 
 export default function NotFound() {
   return (
-    <div className="main-grid-layout w-full flex flex-col justify-between">
-      <div className="flex flex-col max-w-4xl ml-64">
-        <h2 className="font-bold text-5xl text-black-light mt-8 mb-6">
+    <div className="main-grid-layout-vivid w-full flex flex-col justify-between">
+      <div className="grid-container rich-text">
+        <h2 className="font-bold text-5xl mt-8 mb-6 max-w-3xl">
           Oops! Looks like you've wandered off the digital path.
         </h2>
-        <p className="text-2xl text-black-light">
+        <p className="text-2xl max-w-3xl">
           The scenario you're looking for seems to have taken a detour. But fear not! There's plenty
           more to explore.
         </p>
-        <Link className="text-lg text-white mt-8 py-4 px-10 rounded-full bg-violet w-fit" href="/">
+        <Link className="button" href="/">
           Browse all
         </Link>
       </div>
