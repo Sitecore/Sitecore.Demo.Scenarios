a21c7523563065860672c8de3befface6df4ff44
Neli Kostadinova <nko@sitecore.net>
2024-03-18 10:24:42 +0200

    not found fixes

--- src/frontend/app/not-found.tsx
+++ src/frontend/app/not-found.tsx
@@ -4,25 +4,28 @@ import Image from 'next/image';
 export default function NotFound() {
   return (
     <div className="main-grid-layout-vivid w-full flex flex-col justify-between">
-      <div className="grid-container rich-text">
-        <h2 className="font-bold text-5xl mt-8 mb-6 max-w-3xl">
+      <div className="grid-container">
+        <h1 className="text-4xl leading-normal font-bold max-w-[44rem] mb-3 md:text-5xl md:leading-normal">
           Oops! Looks like you've wandered off the digital path.
-        </h2>
-        <p className="text-2xl max-w-3xl">
+        </h1>
+        <p className="text-2xl leading-normal max-w-[44rem]">
           The scenario you're looking for seems to have taken a detour. But fear not! There's plenty
           more to explore.
         </p>
-        <Link className="button" href="/">
+        <Link className="button mt-10" href="/">
           Browse all
         </Link>
       </div>
-      <Image
-        src="/undraw_road_sign_re_3kc3.svg"
-        alt="No scenario found logo"
-        width={760}
-        height={525}
-        unoptimized
-      />
+      <div className="overflow-hidden pt-8">
+        <Image
+          src="/undraw_road_sign_re_3kc3.svg"
+          alt="No scenario found logo"
+          className="max-h-full w-auto"
+          width={760}
+          height={525}
+          unoptimized
+        />
+      </div>
     </div>
   );
 }
