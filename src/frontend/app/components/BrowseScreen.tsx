07dd3c1442be0fbacc77d5e2aef1c131e9565f25
Christos Papoutsakis <chpa@sitecore.net>
2024-03-28 13:48:32 +0200

    Wrap browse screen with WidgetsProvider

--- src/frontend/app/components/BrowseScreen.tsx
+++ src/frontend/app/components/BrowseScreen.tsx
@@ -8,6 +8,8 @@ import { Scenario } from '@/interfaces/scenario';
 import { useEffect } from 'react';
 import { createHasVisitedCookie } from '../actions';
 import ScenarioGridWrapper from './ScenarioGridWrapper';
+import { WidgetsProvider } from '@sitecore-search/react';
+import { config } from '../../services/searchSDK';
 
 export default function BrowseScreen({ scenarios }: { scenarios: Scenario[] | null }) {
   useEffect(() => {
@@ -15,30 +17,32 @@ export default function BrowseScreen({ scenarios }: { scenarios: Scenario[] | nu
   }, []);
 
   return (
-    <main className="main-grid-layout">
-      <div className="h-full flex flex-col gap-6">
-        <section className="grid-container">
-          <h1 className="text-5xl font-bold mb-6">Browse</h1>
-          <div className="relative max-w-96">
-            <input
-              className="w-full rounded-full pl-5 pr-10 pt-1 h-10 shadow-element cursor-pointer focus:outline-none placeholder:text-black-light"
-              type="text"
-              placeholder="Search"
-            />
-            <FontAwesomeIcon icon={faSearch} className="h-4 absolute right-4 bottom-3" />
-          </div>
-        </section>
-        <ScenarioGridWrapper
-          scenarios={scenarios}
-          errorCard={
-            <ErrorCard
-              title="Oops, we haven't found anything."
-              subtitle="Try simplifying your search or drop us a line telling us what you need."
-              button={{ text: 'Contact us', href: CONTACT_US_URL, target: '_blank' }}
-            />
-          }
-        />
-      </div>
-    </main>
+    <WidgetsProvider {...config}>
+      <main className="main-grid-layout">
+        <div className="h-full flex flex-col gap-6">
+          <section className="grid-container">
+            <h1 className="text-5xl font-bold mb-6">Browse</h1>
+            <div className="relative max-w-96">
+              <input
+                className="w-full rounded-full pl-5 pr-10 pt-1 h-10 shadow-element cursor-pointer focus:outline-none placeholder:text-black-light"
+                type="text"
+                placeholder="Search"
+              />
+              <FontAwesomeIcon icon={faSearch} className="h-4 absolute right-4 bottom-3" />
+            </div>
+          </section>
+          <ScenarioGridWrapper
+            scenarios={scenarios}
+            errorCard={
+              <ErrorCard
+                title="Oops, we haven't found anything."
+                subtitle="Try simplifying your search or drop us a line telling us what you need."
+                button={{ text: 'Contact us', href: CONTACT_US_URL, target: '_blank' }}
+              />
+            }
+          />
+        </div>
+      </main>
+    </WidgetsProvider>
   );
 }
