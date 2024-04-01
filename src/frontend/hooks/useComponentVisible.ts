0766ec33967f84f10e4d07392bf5418c0df75da4
Christos Papoutsakis <chpa@sitecore.net>
2024-04-01 16:22:39 +0300

    Add useComponentVisible custom hook

---
+++ src/frontend/hooks/useComponentVisible.ts
@@ -0,0 +1,21 @@
+import { useState, useEffect, useRef } from 'react';
+
+export default function useComponentVisible(isVisibleByDefault: boolean) {
+  const [isComponentVisible, setIsComponentVisible] = useState(isVisibleByDefault);
+  const ref = useRef<HTMLDivElement>(null);
+
+  const handleClickOutside = (event: MouseEvent) => {
+    if (ref.current && !ref.current.contains(event.target as HTMLElement)) {
+      setIsComponentVisible(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('click', handleClickOutside, true);
+    return () => {
+      document.removeEventListener('click', handleClickOutside, true);
+    };
+  }, []);
+
+  return { ref, isComponentVisible, setIsComponentVisible };
+}
