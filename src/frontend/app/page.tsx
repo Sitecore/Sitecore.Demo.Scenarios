f35c7ca1938004148adc613591480da3006df4c2
Christos Papoutsakis <chpa@sitecore.net>
2024-04-03 18:46:56 +0300

    Skip intro screen if user coming directly from demo portal

--- src/frontend/app/page.tsx
+++ src/frontend/app/page.tsx
@@ -1,15 +1,23 @@
 import { cookies } from 'next/headers';
+
 import { getAllScenarios } from '@/api/queries/scenarios';
 import IntroScreen from './components/IntroScreen';
 import BrowseScreen from './components/BrowseScreen';
 import NavBar from './components/NavBar';
 
-export default async function Home() {
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | string[] | undefined };
+}) {
   const scenarios = await getAllScenarios();
   const cookieStore = cookies();
   const hasCookie = cookieStore.has('hasVisited');
 
-  return hasCookie ? (
+  const isDemoPortalUser = Object.keys(searchParams).length > 0;
+
+  // Skip intro screen if the user has visited before or if coming directly from the Demo Portal
+  return hasCookie || isDemoPortalUser ? (
     <>
       <NavBar />
       <BrowseScreen scenarios={scenarios} />
