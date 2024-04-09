aa052d72e05d6734d00a0560b8ffbf380ab29d3c
Christos Papoutsakis <chpa@sitecore.net>
2024-04-09 15:20:31 +0300

    Add demo portal notification

--- src/frontend/app/components/BrowseScreen.tsx
+++ src/frontend/app/components/BrowseScreen.tsx
@@ -2,17 +2,25 @@
 
 import { useCallback, useEffect, useState } from 'react';
 import { WidgetsProvider } from '@sitecore-search/react';
+import { faRemove } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-import { CONTACT_US_URL } from '@/constants/scenario';
+import { CONTACT_US_URL, DEMO_PORTAL_USER_KEY } from '@/constants/scenario';
 import ErrorCard from '../components/ErrorCard';
 import { Scenario } from '@/interfaces/scenario';
 import { createHasVisitedCookie } from '../actions';
 import ScenarioGridWrapper from './ScenarioGridWrapper';
 import { config } from '../../services/searchSDK';
 import BrowseScreenSearchWidget from './BrowseScreenSearchWidget';
+import { useSearchParams } from 'next/navigation';
 
 export default function BrowseScreen({ scenarios }: { scenarios: Scenario[] | null }) {
+  const searchParams = useSearchParams();
+
   const [filteredScenarios, setFilteredScenarios] = useState(scenarios ?? []);
+  const [showDemoPortalNotification, setShowDemoPortalNotification] = useState(
+    searchParams.get(DEMO_PORTAL_USER_KEY) === 'true'
+  );
 
   const updateFilteredScenarios = useCallback(
     (filteredScenarioIDs: string[]) => {
@@ -35,6 +43,17 @@ export default function BrowseScreen({ scenarios }: { scenarios: Scenario[] | nu
             <h1 className="text-5xl font-bold mb-6">Browse</h1>
             <BrowseScreenSearchWidget rfkId="rfkid_7" onFilterScenarios={updateFilteredScenarios} />
           </section>
+          {showDemoPortalNotification && (
+            <section className="absolute top-6 right-6 px-9 py-6 rounded-lg bg-white text-black-light border-l-4 border-l-violet animate-[bounce_2s_ease-in-out_2]">
+              <h3 className="font-bold text-xl">You seem to be coming from the Demo Portal!</h3>
+              <p>We&apos;ve added some filters to help you get started with your demo.</p>
+              <FontAwesomeIcon
+                className="h-4 w-4 align-middle absolute top-3 right-3 cursor-pointer"
+                icon={faRemove}
+                onClick={() => setShowDemoPortalNotification(false)}
+              />
+            </section>
+          )}
           <ScenarioGridWrapper
             scenarios={filteredScenarios}
             errorCard={
--- src/frontend/app/page.tsx
+++ src/frontend/app/page.tsx
@@ -4,6 +4,7 @@ import { getAllScenarios } from '@/api/queries/scenarios';
 import IntroScreen from './components/IntroScreen';
 import BrowseScreen from './components/BrowseScreen';
 import NavBar from './components/NavBar';
+import { DEMO_PORTAL_USER_KEY } from '@/constants/scenario';
 
 export default async function Home({
   searchParams,
@@ -14,7 +15,7 @@ export default async function Home({
   const cookieStore = cookies();
   const hasCookie = cookieStore.has('hasVisited');
 
-  const isDemoPortalUser = Object.keys(searchParams).length > 0;
+  const isDemoPortalUser = searchParams[DEMO_PORTAL_USER_KEY] === 'true';
 
   // Skip intro screen if the user has visited before or if coming directly from the Demo Portal
   return hasCookie || isDemoPortalUser ? (
