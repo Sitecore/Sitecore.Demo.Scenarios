6513fcf86818a07196aa1722af3b6ac9629afbc6
Neli Kostadinova <110887014+sc-nelikostadinova@users.noreply.github.com>
2024-04-16 09:44:12 +0300

    AB#9285 CTA Banner (#101)

--- src/frontend/app/components/ScenarioGrid.tsx
+++ src/frontend/app/components/ScenarioGrid.tsx
@@ -3,16 +3,32 @@
 import { MouseEvent, useCallback, useEffect, useRef } from 'react';
 import Link from 'next/link';
 import { useParams, useRouter, useSearchParams } from 'next/navigation';
-
 import { CategoryOptions, Scenario } from '@/interfaces/scenario';
 import BookmarkIcon from './BookmarkIcon';
 import TagList from './TagList';
 import { useSidebarContext } from '../context/sidebar';
 import { getElementPosition, isElementInView } from '@/helpers/ui';
+import CTABanner from './CTABanner';
 
 type ScenarioGridProps = {
   scenarios: Scenario[];
 };
+
+// Split the scenario array into two parts to insert the banner between them
+function splitItems(items: Scenario[]) {
+  const totalLength = items.length;
+
+  let firstPartLength = Math.floor((totalLength * 3) / 4);
+
+  if (firstPartLength % 3 !== 0) {
+    firstPartLength += 3 - (firstPartLength % 3);
+  }
+
+  const firstPart = items.slice(0, firstPartLength);
+  const secondPart = items.slice(firstPartLength);
+
+  return [firstPart, secondPart];
+}
 
 export default function ScenarioGrid({ scenarios }: ScenarioGridProps) {
   const router = useRouter();
@@ -20,6 +36,8 @@ export default function ScenarioGrid({ scenarios }: ScenarioGridProps) {
   const params = useParams<{ id: string }>();
   const scrollRef = useRef<HTMLAnchorElement>(null);
   const { page, scrollPos, setScrollPos } = useSidebarContext();
+
+  const [firstScenarios, restScenarios] = splitItems(scenarios);
 
   // Adjust sidebar scroll to show the selected scenario after page reload
   useEffect(() => {
@@ -54,9 +72,9 @@ export default function ScenarioGrid({ scenarios }: ScenarioGridProps) {
     [router]
   );
 
-  return (
-    <div className="flex flex-wrap gap-6 py-4 grid-container">
-      {scenarios.map((scenario) => (
+  const scenarioCard = useCallback(
+    (scenario: Scenario) => {
+      return (
         <Link
           key={scenario.id}
           className="group relative bg-white rounded-lg shadow-card py-10 px-8 basis-[calc(33.33%-1rem)] flex-grow min-w-96 transition-all cursor-pointer hover:shadow-card-hover"
@@ -78,7 +96,20 @@ export default function ScenarioGrid({ scenarios }: ScenarioGridProps) {
           <p className="line-clamp-3 mb-3">{scenario.summary}</p>
           <TagList scenario={scenario} />
         </Link>
-      ))}
+      );
+    },
+    [handleScenarioClick, params.id, searchParams]
+  );
+
+  return (
+    <div className="flex flex-wrap gap-6 py-4 grid-container">
+      {firstScenarios.map((scenario) => scenarioCard(scenario))}
+      <CTABanner
+        title="Looking for something that's not here?"
+        subtitle="Drop us a line at demo@sitecore.com and tell us what you're after"
+        button={{ text: 'Contact us', href: 'mailto:demo@sitecore.com' }}
+      />
+      {restScenarios.map((scenario) => scenarioCard(scenario))}
       <div className="basis-[calc(33.33%-1rem)] flex-grow min-w-96"></div>
       <div className="basis-[calc(33.33%-1rem)] flex-grow min-w-96"></div>
     </div>
